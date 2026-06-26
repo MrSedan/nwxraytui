96d3934df04f0146645a01a139e27a1c14470e4a
Sergey Elpashev <elpashevs@bk.ru>
2026-06-26 11:43:32 +0300

    feat: main entrypoint with --daemon flag

--- cmd/nwxraytui/main.go
+++ cmd/nwxraytui/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/mrsedan/nwxraytui/internal/config"
+	"github.com/mrsedan/nwxraytui/internal/daemon"
+	"github.com/mrsedan/nwxraytui/internal/ipc"
+	"github.com/mrsedan/nwxraytui/internal/tui"
+)
 
 func main() {
-	fmt.Println("nwxraytui")
+	isDaemon := flag.Bool("daemon", false, "run as background daemon")
+	flag.Parse()
+
+	cfgPath := config.DefaultPath()
+	cfg, err := config.Load(cfgPath)
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	if *isDaemon {
+		runDaemon(cfg)
+		return
+	}
+	runTUI(cfg)
+}
+
+func runDaemon(cfg *config.Config) {
+	xrayBin, err := exec.LookPath("xray")
+	if err != nil {
+		log.Fatalf("xray not found in PATH: %v", err)
+	}
+	sock := config.SocketPath()
+	d := daemon.New(cfg, xrayBin)
+	if err := d.Run(sock); err != nil {
+		log.Fatalf("daemon: %v", err)
+	}
+}
+
+func runTUI(cfg *config.Config) {
+	sock := config.SocketPath()
+
+	client, err := ipc.NewClient(sock)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Daemon not running. Starting...")
+		go func() {
+			cmd := exec.Command(os.Args[0], "--daemon")
+			cmd.Start()
+		}()
+		time.Sleep(500 * time.Millisecond)
+		client, err = ipc.NewClient(sock)
+		if err != nil {
+			log.Fatalf("cannot connect to daemon: %v", err)
+		}
+	}
+	defer client.Close()
+
+	app := tui.New(client)
+	if err := app.Start(); err != nil {
+		log.Fatalf("TUI: %v", err)
+	}
 }

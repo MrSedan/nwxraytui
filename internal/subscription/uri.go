8b11718d9ad2956c6c71eb222d232926ba6fa132
Sergey Elpashev <elpashevs@bk.ru>
2026-06-26 11:32:32 +0300

    feat: subscription JSON parser

---
+++ internal/subscription/uri.go
@@ -0,0 +1,8 @@
+package subscription
+
+import "fmt"
+
+// ParseURI is implemented in Task 5.
+func ParseURI(uri string) (Server, error) {
+	return Server{}, fmt.Errorf("ParseURI: not implemented")
+}
